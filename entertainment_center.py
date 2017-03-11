707231402c057e5c2e7935b9954784a03134d4f9
Roman G <roman@harmonypark.net>
2017-03-11 10:59:35 +0000

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,8 @@
 import media
 
-toy_story = media.Movie("Toy Story", "This article is about the original Toy Story film.", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://youtu.be/KYz2wyBy3kc")
+toy_story = media.Movie("Toy Story", 
+                        "This article is about the original Toy Story film.", 
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", 
+                        "https://youtu.be/KYz2wyBy3kc")
 
 print(toy_story.storyline)
